a01931ac47b50ec89253c18502bb36458bf9ac2d
dmxyws <mr_lee_007@sina.com>
2025-05-13 16:11:36 +0800

    feat: 引入Pinia、数据持久化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia' // 引入Pinia
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate' // 引入数据持久化插件
 // import './style.css'
 import 'element-plus/dist/index.css' // Element Plus样式
 import 'normalize.css' // Normalize.css必须在Element样式之后引入
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia() // 创建Pinia实例
+pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
+
+app.use(pinia) // 将Pinia安装到应用中
 app.use(router)
 app.mount('#app')
