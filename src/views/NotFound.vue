306755dd4fe7240f0216aeb185f1a2a633ecfa10
dmxyws <mr_lee_007@sina.com>
2025-05-13 15:46:59 +0800

    refactor: 404页面优化

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen p-4">
-    <h1 class="text-[clamp(2rem,5vw,4rem)] font-bold text-gray-800 mb-4">
-      404
-    </h1>
-    <p class="text-xl text-gray-600 mb-6">Page Not Found</p>
-    <router-link
-      to="/home"
-      class="px-4 py-2 bg-primary hover:bg-primary/90 text-white rounded-lg transition-all"
-    >
-      Return to Home
-    </router-link>
+  <div class="page404">
+    <div class="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 class="text-[clamp(2rem,5vw,4rem)] font-bold text-gray-800 mb-4">
+        404
+      </h1>
+      <p class="text-xl text-gray-600 mb-6">Page Not Found</p>
+      <router-link
+        to="/home"
+        class="px-4 py-2 bg-primary hover:bg-primary/90 text-white rounded-lg transition-all"
+      >
+        Return to Home
+      </router-link>
+    </div>
   </div>
 </template>
 
 <style scoped>
-:root {
-  --primary: #165dff;
+.page404 {
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
