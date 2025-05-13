680cdbf994b6d586a09691e1102675ae14ad8797
dmxyws <mr_lee_007@sina.com>
2025-05-13 15:35:25 +0800

    feat: 引入 normalize.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
+import 'element-plus/dist/index.css' // Element Plus样式
+import 'normalize.css' // Normalize.css必须在Element样式之后引入
 import App from './App.vue'
 import router from './router'
 
